75837b902cc851563e03e5d414317591722b96da
Valient Gough <vgough@pobox.com>
2012-10-22 05:30:55 +0000

    apply license change
    git-svn-id: http://encfs.googlecode.com/svn/branches/1.7.x_lgpl@91 db9cf616-1c43-0410-9cb8-a902689de0d6

--- encfs/base64.cpp
+++ encfs/base64.cpp
@@ -4,16 +4,18 @@
  *****************************************************************************
  * Copyright (c) 2002-2004, Valient Gough
  *
- * This library is free software; you can distribute it and/or modify it under
- * the terms of the GNU General Public License (GPL), as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GPL in the file COPYING for more
- * details.
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
  *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "base64.h"
