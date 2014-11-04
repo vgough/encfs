8efda5b7009a445fc510da166dfb66e54a1d4a6f
Jakob Unterwurzacher <jakobunt@gmail.com>
2014-11-04 23:08:33 +0100

    tests: Check that absolute symlinks work in reverse mode

--- tests/reverse.pl
+++ tests/reverse.pl
@@ -58,9 +58,23 @@ sub copy_test
     ok(! -f "$decrypted/encfs.cpp", "file deleted");
 }
 
+# Create symlinks and verify they are correctly decrypted
+# Parameter: symlink target
+sub symlink_test
+{
+    my $target = shift(@_);
+    symlink($target, "$plain/symlink");
+    ok( readlink("$decrypted/symlink") eq "$target", "symlink to '$target'");
+    unlink("$plain/symlink");
+}
+
 newWorkingDir();
 mount();
 
 copy_test();
+symlink_test("/"); # absolute
+symlink_test("foo"); # relative
+symlink_test("/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/15/17/18"); # long
+symlink_test("!§\$%&/()\\<>#+="); # special characters
 
 cleanup();
