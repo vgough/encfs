05a445d65c615e6fa9d2f8aed6b37d9e3b8533eb
Valient Gough <vgough@pobox.com>
2015-08-19 20:13:14 -0700

    use default cmake install for circle ci test

--- ci/config.sh
+++ ci/config.sh
@@ -2,4 +2,4 @@ set -x
 set -e
 mkdir build
 cd build
-../ci/cmake/bin/cmake -DCMAKE_INSTALL_PREFIX:PATH=/tmp/encfs -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/tmp/encfs -DCMAKE_BUILD_TYPE=Debug ..
