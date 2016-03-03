10c231d172fd3137e2c7288ee78a429b7fd95485
Julien-Pierre Avérous <github@sourcemac.com>
2016-03-03 17:06:27 +0100

    Fix compilation for OS X.

--- fs/RawFileIO.cpp
+++ fs/RawFileIO.cpp
@@ -245,7 +245,7 @@ int RawFileIO::truncate(off_t size) {
 
   if (fd >= 0 && canWrite) {
     res = ::ftruncate(fd, size);
-#ifndef __FreeBSD__
+#if !defined(__FreeBSD__) && !defined(__APPLE__)
     ::fdatasync(fd);
 #endif
   } else
