aaed0f0f18b4e553198950530d817174ab31b166
benrubson <ben.rubson@gmail.com>
2017-02-16 17:37:35 +0100

    Update message severity

--- encfs/main.cpp
+++ encfs/main.cpp
@@ -725,8 +725,8 @@ static void *idleMonitor(void *_arg) {
       ++idleCycles;
     else {
       if (idleCycles >= timeoutCycles)
-        RLOG(WARNING) << "Filesystem no longer inactive: "
-                      << arg->opts->mountPoint;
+        RLOG(INFO) << "Filesystem no longer inactive: "
+                   << arg->opts->mountPoint;
       idleCycles = 0;
     }
 
