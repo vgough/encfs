075b97e34c99f8de55747f88782f9cdfe859b52e
benrubson <ben.rubson@gmail.com>
2017-04-11 09:43:34 +0200

    Make cmake flawlessly run with Debian jessie cmake 3.0.2

--- ci/install-cmake.sh
+++ ci/install-cmake.sh
@@ -1,7 +1,8 @@
 set -x
 set -e
 if [ ! -e ci/cmake/bin/cmake ]; then
-  wget http://www.cmake.org/files/v3.2/cmake-3.2.0-Linux-x86_64.tar.gz
-  tar -xzf cmake-3.2.0-Linux-x86_64.tar.gz
-  mv cmake-3.2.0-Linux-x86_64 ci/cmake
+  # First x86_64 cmake compiled version
+  wget http://www.cmake.org/files/v3.1/cmake-3.1.0-Linux-x86_64.tar.gz
+  tar -xzf cmake-3.1.0-Linux-x86_64.tar.gz
+  mv cmake-3.1.0-Linux-x86_64 ci/cmake
 fi
