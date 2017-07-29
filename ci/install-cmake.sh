6ed2001c2e7682ef5e968b8f165c927c535f2d77
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-07-29 19:09:24 +0200

    travis ci: upgrade to trusty, use system cmake
    On July 18, Travis upgraded their default build VM
    to Ubuntu Trusty. Trusty has cmake 3.5.1, which allows us to
    drop our bundled version.
    
    We explicitely set "dist: trusty" to prevent our build jobs
    from being routed to an older build VM.

--- ci/install-cmake.sh
+++
@@ -1,7 +0,0 @@
-set -x
-set -e
-if [ ! -e ci/cmake/bin/cmake ]; then
-  #wget http://www.cmake.org/files/v3.1/cmake-3.1.0-Linux-x86_64.tar.gz
-  tar -xzf ci/cmake-3.0.2-Linux-x86_64.tar.gz
-  mv cmake-3.0.2-Linux-x86_64 ci/cmake
-fi
