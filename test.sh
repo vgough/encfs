8a2c03d8a9432586d24d85d0276dace856eb24f3
Jakob Unterwurzacher <jakobunt@gmail.com>
2017-07-29 21:07:08 +0200

    travis ci: enable gcc, call build.sh && test.sh
    gcc is default compiler on most distributions, we should
    also test with it.
    
    build.sh exists and should be tested as well, so why not use
    it in Travis.
    test.sh outputs much more details than "make check", so
    run it as well.
    
    Additionally, make build.sh and test.sh work when called from
    other directories.

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -eu
 
-./build/checkops &> /dev/null
+# Make sure we are in the directory this script is in.
+cd "$(dirname "$0")"
 
+# Failed tests can leave dangling mounts behind.
 for i in $(mount | grep -e "/tmp/encfs-reverse-tests-\|/tmp/encfs-tests-" | cut -f3 -d" "); do
 	echo "Warning: unmounting leftover filesystem: $i"
-	fusermount -u $i
+	fusermount -u $i || true
 done
+
+# This is very noisy so run it silently at first. Run it again with
+# output if the first run fails.
+./build/checkops &> /dev/null || ./build/checkops
 
 perl -MTest::Harness -e '$$Test::Harness::debug=1; runtests @ARGV;' tests/*.t.pl
