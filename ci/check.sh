a8eebf3d8ec44c784681e63b60837f3ec5160d9a
Valient Gough <valient@gmail.com>
2017-08-08 00:38:10 -0400

    special case Linux Travis setup

--- ci/check.sh
+++ ci/check.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -eu
 
+cmake --version
+
+CFG=""
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+  CFG="-DENABLE_NLS=OFF -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl"
+fi
+
 if [[ ! -d build ]]
 then
-	mkdir build
+  mkdir build
 fi
 
 cd build
-cmake ..
+cmake .. ${CFG}
 make -j2
 make test
 
