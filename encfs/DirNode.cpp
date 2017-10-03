1e9996c680cfb9ac0a8352277d506cd5defbefa2
benrubson <ben.rubson@gmail.com>
2017-10-03 16:02:31 +0200

    Do things in the correct order

--- encfs/DirNode.cpp
+++ encfs/DirNode.cpp
@@ -520,6 +520,13 @@ int DirNode::mkdir(const char *plaintextPath, mode_t mode, uid_t uid,
 
   int res = ::mkdir(cyName.c_str(), mode);
 
+  if (res == -1) {
+    int eno = errno;
+    RLOG(WARNING) << "mkdir error on " << cyName << " mode " << mode << ": "
+                  << strerror(eno);
+    res = -eno;
+  }
+
   if (olduid >= 0) {
     if(setfsuid(olduid) == -1) {
       int eno = errno;
@@ -533,15 +540,6 @@ int DirNode::mkdir(const char *plaintextPath, mode_t mode, uid_t uid,
       RLOG(DEBUG) << "setfsgid back error: " << strerror(eno);
       // does not return error here as initial setfsgid worked
     }
-  }
-
-  if (res == -1) {
-    int eno = errno;
-    RLOG(WARNING) << "mkdir error on " << cyName << " mode " << mode << ": "
-                  << strerror(eno);
-    res = -eno;
-  } else {
-    res = 0;
   }
 
   return res;
