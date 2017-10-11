cc3d4e8cff775c415704928f625c0dc3fc54d0bf
benrubson <ben.rubson@gmail.com>
2017-10-11 11:20:39 +0200

    Correct last clang warnings in readpassphrase

--- encfs/readpassphrase.cpp
+++ encfs/readpassphrase.cpp
@@ -126,29 +126,30 @@ restart:
   }
 
   if (write(output, prompt, strlen(prompt)) != -1) {
-    end = buf + bufsiz - 1;
-    for (p = buf; (nr = read(input, &ch, 1)) == 1 && ch != '\n' && ch != '\r';) {
-      if (p < end) {
-        if ((flags & RPP_SEVENBIT) != 0) {
-          ch &= 0x7f;
-        }
-        if (isalpha(ch) != 0) {
-          if ((flags & RPP_FORCELOWER) != 0) {
-            ch = tolower(ch);
-          }
-          if ((flags & RPP_FORCEUPPER) != 0) {
-            ch = toupper(ch);
-          }
-        }
-        *p++ = ch;
-      }
-    }
-    *p = '\0';
+    //dummy test to get rid of warn_unused_result compilation warning
   }
+  end = buf + bufsiz - 1;
+  for (p = buf; (nr = read(input, &ch, 1)) == 1 && ch != '\n' && ch != '\r';) {
+    if (p < end) {
+      if ((flags & RPP_SEVENBIT) != 0) {
+        ch &= 0x7f;
+      }
+      if (isalpha(ch) != 0) {
+        if ((flags & RPP_FORCELOWER) != 0) {
+          ch = tolower(ch);
+        }
+        if ((flags & RPP_FORCEUPPER) != 0) {
+          ch = toupper(ch);
+        }
+      }
+      *p++ = ch;
+    }
+  }
+  *p = '\0';
   save_errno = errno;
   if ((term.c_lflag & ECHO) == 0u) {
-    if(write(output, "\n", 1) != -1) {
-    	//dummy test to get rid of warn_unused_result compilation warning
+    if (write(output, "\n", 1) != -1) {
+      //dummy test to get rid of warn_unused_result compilation warning
     }
   }
 
