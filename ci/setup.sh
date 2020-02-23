ee47e2fc140cd30cc033c7844555247075a73bf4
Ben RUBSON <6764151+benrubson@users.noreply.github.com>
2020-02-23 16:51:15 +0100

    Add FreeBSD Cirrus CI (#580)

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,6 +5,8 @@
 if [[ "$INTEGRATION" == "true" ]]; then
   if uname -s | grep -q Linux; then
     sudo modprobe fuse
+  elif uname -s | grep -q FreeBSD; then
+    kldload fuse
   elif uname -s | grep -q Darwin; then
     brew cask install osxfuse
     brew reinstall openssl
