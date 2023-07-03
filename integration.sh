c5e5651f02b8aa7a5acd1737253780decf71636c
Sau Chow <sauchow@sandiego.edu>
2023-07-03 00:51:48 -0700

    fiddle with the tests

--- integration.sh
+++ integration.sh
@@ -3,4 +3,22 @@
 # Make sure we are in the directory this script is in.
 cd "$(dirname "$0")"
 
-perl -I. -MTest::Harness -e '$$Test::Harness::debug=1; runtests @ARGV;' integration/*.t.pl
+if [ -d build ] ; then
+    cd build
+else
+    mkdir build
+    cd build
+    cmake ..
+fi
+make -j$(nproc)
+cd ..
+
+#if [ ! -d ~/fuse-xfstests ] ; then
+#    cd ~
+#    git clone https://github.com/rfjakob/fuse-xfstests
+#    cd fuse-xfstests
+#    make
+#    cd "$(dirname "$0")"
+#fi
+
+perl -I. -MTest::Harness -e '$Test::Harness::debug=1; $Test::Harness::verbose=1; runtests @ARGV;' integration/*.t.pl
