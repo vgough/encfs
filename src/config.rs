da9afa95240d1c142f0fc1ae4452573a94fddcc3
Jakob Unterwurzacher <jakobunt@gmail.com>
2026-02-22 21:20:35 +0100

    config: fsync config file
    Three improvements:
    
    1) write(2) errors are detected now, because writer.into_inner()
       flushes BufWriter's buffer
    2) Errors on close(2) don't matter anymore. Before, they were not
       detected at all (see 3rd paragraph at
       https://doc.rust-lang.org/std/fs/struct.File.html ) and could
       cause data loss
    3) File is safely on disk even if the PC crashes before the underlying
       filesystem decides to flush it's writeback buffers

--- src/config.rs
+++ src/config.rs
@@ -907,6 +907,10 @@ impl EncfsConfig {
         // Append a newline at the end
         writer.write_all(b"\n")?;
 
+        // Ensure the file is safely on disk
+        let file = writer.into_inner().context("flush failed")?;
+        file.sync_all().context("fsync failed")?;
+
         Ok(())
     }
 
@@ -971,6 +975,11 @@ impl EncfsConfig {
         writer
             .write_all(&buf)
             .context("Failed to write V7 config")?;
+
+        // Ensure the file is safely on disk
+        let file = writer.into_inner().context("flush failed")?;
+        file.sync_all().context("fsync failed")?;
+
         Ok(())
     }
 
